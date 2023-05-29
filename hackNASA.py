bd3cccbb3bebac1cb70d08322f2e57f3da51371d
Walker <daldridge@lairdscomputer.com>
2023-05-28 21:01:59 -0500

    Nasa hacking spoof

---
+++ hackNASA.py
@@ -0,0 +1,19 @@
+# Hack NASA Spoof
+import time
+
+print("Start Attacking........")
+print("Hacking NASA 0%")
+time.sleep(1)
+print("Hacking NASA 20%")
+time.sleep(1)
+print("Hacking NASA 40%")
+time.sleep(1)
+print("Hacking NASA 60%")
+time.sleep(1)
+print("Hacking NASA 70%")
+time.sleep(2) 	#drama
+print("Hacking NASA 80%")
+time.sleep(6)	#DRAMA
+print("Hacking NASA 100%")
+time.sleep(1)
+print("NASA Hacked Successfully")
