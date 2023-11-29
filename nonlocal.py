8773ab4369c8aea64d4df094f7f7ebe141ff911c
Walker Aldridge <daldridge@lairdscomputer.com>
2023-11-28 23:28:53 -0600

    example of nonlocal keyword

---
+++ nonlocal.py
@@ -0,0 +1,13 @@
+def counter(start=0):
+	count=start
+	def inc():
+		nonlocal count
+		count += 1
+		return count
+	return inc
+    
+count1 = counter(5)
+count2 = counter()
+result = count1() + count2()
+
+print(result)
