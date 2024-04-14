9f56de5301c740e27cee91e715968fbe660b45d3
Walker Aldridge <daldridge@lairdscomputer.com>
2024-04-13 21:21:25 -0500

    Globals

--- globals.py
+++ globals.py
@@ -1,12 +1,12 @@
 # Fun with globals
 counter = 0
 
-def increment_counter(counter):
+def increment_counter():
     globals()["counter"] += 1
 
 print(counter)
-increment_counter(counter)
+increment_counter()
 print(counter)
-increment_counter(counter)
+increment_counter()
 print(counter)
-increment_counter(counter)
+increment_counter()
