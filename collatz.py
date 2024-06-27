3a7f02464cee683691b1203ef2a1d90c5d1ad2af
Walker Aldridge <daldridge@lairdscomputer.com>
2024-06-27 09:28:33 -0500

    Memoized Collatz

--- collatz.py
+++ collatz.py
@@ -1,13 +1,5 @@
 # Collatz
 import time
-import threading
-
-
-class ThreadWithResult(threading.Thread):
-    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, *, daemon=None):
-        def function():
-            self.result = target(*args, **kwargs)
-        super().__init__(group=group, target=function, name=name, daemon=daemon)
 
 
 def collatz(seed: int):
@@ -32,23 +24,9 @@ def collatz(seed: int):
 esteps = 0
 memos = [0] * 10_000_000
 start_time = time.time()
-for i in range(1, 10_000_000, 4):
-    t1 = ThreadWithResult(target=collatz, args=(i,))
-    t1.start()
-    t2 = ThreadWithResult(target=collatz, args=(i+1,))
-    t2.start()
-    t3 = ThreadWithResult(target=collatz, args=(i+2,))
-    t3.start()
-    t4 = ThreadWithResult(target=collatz, args=(i+3,))
-    t4.start()
-    t1.join()
-    memos[i] = t1.result
-    t2.join()
-    memos[i+1] = t2.result
-    t3.join()
-    memos[i+2] = t3.result
-    t4.join()
-    memos[i+4] = t4.result
+for i in range(1, 10_000_000):
+    esteps = collatz(i)
+    memos[i] = esteps
     if i % 1000000 == 0:
         print(f'Seed:{i} Steps:{esteps}')
 elapsed = time.time() - start_time
