4af3ee7ce6ef104d022e8908bfd516c5fc3b6db8
Walker Aldridge <daldridge@lairdscomputer.com>
2024-09-10 09:32:01 -0500

    Upload using oauth lib

--- txupload.py
+++ txupload.py
@@ -1,6 +1,7 @@
 import json
 import os
 import requests
+from requests_oauth2client import OAuth2Client, BearerToken
 
 # Globals
 auth_endpoint = "https://texas-sos-stage.appiancloud.com/suite/authorization/oauth/token"
@@ -10,24 +11,15 @@ client_secret = os.getenv("CLIENT_SECRET")
 
 
 # Get Token
-def get_token(cid: str, secret: str, endpoint: str, target: str) -> {}:
-    response = requests.post(endpoint, data={
-        'address': target,
-        'client_id': cid,
-        'client_secret': secret,
-        'grant_type': 'client_credentials'
-    },
-    headers={
-        'Accept': 'application/json'
-    })
-    return json.loads(response.text)
+def get_token(cid: str, secret: str, endpoint: str, target: str) -> BearerToken:
+    oa_client = OAuth2Client(token_endpoint=endpoint, client_id=cid, client_secret=secret)
+    retval = oa_client.client_credentials(resource=target)
+    return retval
 
 
 # Submit List
-def submit_application(token: str, body: any, endpoint: str) -> str:
-    response = requests.post(endpoint, json=body, headers={
-        'UserToken': token
-    })
+def submit_application(token: BearerToken, body: any, endpoint: str) -> str:
+    response = requests.post(endpoint, json=body, auth=token)
     return response.text
 
 
@@ -38,8 +30,8 @@ with open('/Users/walker/Downloads/SOSSingleUpload.json') as f:
 
 print('Getting Token')
 token = get_token(client_id, client_secret, auth_endpoint, upload_endpoint)
-print(f'Token: {token["access_token"]}')
+print(f'Token: {token.access_token}')
 
 print('Submitting Application')
-result = submit_application(token["access_token"], payload, upload_endpoint)
+result = submit_application(token, payload, upload_endpoint)
 print(result)
