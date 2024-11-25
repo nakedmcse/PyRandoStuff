6a9ed60933efb53aa0d403d65d47b51b43f05923
Walker Aldridge <daldridge@lairdscomputer.com>
2024-11-24 23:50:35 -0600

    AoC 2023 Day 24 Part 2

---
+++ 2023_day24_part2.py
@@ -0,0 +1,102 @@
+# 2023 Day24 Part 2
+import math
+import re
+
+
+class vector:
+    def __init__(self, x: int, y: int, z: int):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return f'{self.x}, {self.y}, {self.z}'
+
+
+class hail:
+    def __init__(self, position: vector, velocity: vector):
+        self.position = position
+        self.velocity = velocity
+
+    def __repr__(self):
+        return f'{self.position.x}, {self.position.y}, {self.position.z} @ {self.velocity.x}, {self.velocity.y}, {self.velocity.z} ({self.speed()})'
+
+    def speed(self) -> float:
+        return math.sqrt(self.velocity.x**2 + self.velocity.y**2 + self.velocity.z**2)
+
+    def increment_position(self):
+        self.position.x += self.velocity.x
+        self.position.y += self.velocity.y
+        self.position.z += self.velocity.z
+
+    def set_time_index(self, ti: int):
+        self.position.x += (self.velocity.x * ti)
+        self.position.y += (self.velocity.y * ti)
+        self.position.z += (self.velocity.z * ti)
+
+
+def subtract_vectors(a: vector, b: vector) -> vector:
+    return vector(a.x - b.x, a.y - b.y, a.z - b.z)
+
+
+def cross_product(a: vector, b: vector) -> vector:
+    return vector((a.y*b.z) - (a.z*b.y), (a.z*b.x) - (a.x*b.z), (a.x*b.y) - (a.y*b.x))
+
+
+def on_same_line(a: vector, b: vector, c: vector) -> bool:
+    vector_ab = subtract_vectors(b, a)
+    vector_ac = subtract_vectors(c, a)
+    cross = cross_product(vector_ab, vector_ac)
+    return cross.x == 0 and cross.y == 0 and cross.z == 0
+
+
+def search_vector_history(h1: list[vector], h2: list[vector], h3: list[vector]) -> list[int] | None:
+    for f in range(len(h1)):
+        for m in range(len(h2)):
+            for l in range(len(h3)):
+                if on_same_line(h1[f], h2[m], h3[l]):
+                    return [f, m, l]
+    return None
+
+
+with open('2023_day24_test.txt') as file:
+    hail_list = [hail(vector(int(y[0]), int(y[1]), int(y[2])), vector(int(y[3]), int(y[4]), int(y[5])))
+                 for y in (re.findall(r'-?\d+', x) for x in file.read().splitlines())]
+
+hail_list.sort(key=lambda x: x.speed())
+
+first_hail = hail_list[0]
+mid_hail = hail_list[1]
+last_hail = hail_list[2]
+
+#first_hail.set_time_index(5673290541700)
+#mid_hail.set_time_index(5673290541700)
+#last_hail.set_time_index(5673290541700)
+
+first_hail_history = []
+mid_hail_history = []
+last_hail_history = []
+
+i = 0
+while i < 500:
+    first_hail_history.append(vector(first_hail.position.x, first_hail.position.y, first_hail.position.z))
+    mid_hail_history.append(vector(mid_hail.position.x, mid_hail.position.y, mid_hail.position.z))
+    last_hail_history.append(vector(last_hail.position.x, last_hail.position.y, last_hail.position.z))
+    first_hail.increment_position()
+    mid_hail.increment_position()
+    last_hail.increment_position()
+    i += 1
+
+result = search_vector_history(first_hail_history, mid_hail_history, last_hail_history)
+if result:
+    print(f'Line found at time indexes {result[0]}, {result[1]}, {result[2]}')
+    time_diff = result[1] - result[0]
+    stone_velocity = vector((mid_hail_history[result[1]].x - first_hail_history[result[0]].x)//time_diff,
+                            (mid_hail_history[result[1]].y - first_hail_history[result[0]].y)//time_diff,
+                            (mid_hail_history[result[1]].z - first_hail_history[result[0]].z)//time_diff)
+    stone_position = vector(first_hail_history[result[0]].x - (result[0] * stone_velocity.x),
+                            first_hail_history[result[0]].y - (result[0] * stone_velocity.y),
+                            first_hail_history[result[0]].z - (result[0] * stone_velocity.z))
+    print(f'Position: {stone_position} Velocity: {stone_velocity}')
+else:
+    print('No solution found')
