528c49476dc0166c9c3ad007b6568a2b8a5ef659
Walker Aldridge <daldridge@lairdscomputer.com>
2025-09-16 19:38:12 -0500

    Winning prize strings solution space brute force

--- prizestrings.py
+++ prizestrings.py
@@ -1,8 +1,9 @@
 # Project Euler 191
 # https://projecteuler.net/problem=191
+import time
 
+# Reverse brute force in solution space
 winning_combos = []
-
 def get_initial_winning_combos():
     global winning_combos
     options = ['o','l','a']
@@ -12,7 +13,10 @@ def get_initial_winning_combos():
                 combo = a+b+c
                 if 'aaa' in combo: continue
                 if combo.count('l') > 1: continue
-                winning_combos.append(combo)
+                a_con_count = 0
+                if c == 'a' and b == 'a': a_con_count = 2
+                elif c == 'a' and b != 'a': a_con_count = 1
+                winning_combos.append((a_con_count, combo.count('l')))
 
 def extend_winning_combos():
     global winning_combos
@@ -20,16 +24,20 @@ def extend_winning_combos():
     options = ['o','l','a']
     for a in options:
         for c in winning_combos:
-            newcombo = c+a
-            if 'aaa' in newcombo: continue
-            if newcombo.count('l') > 1: continue
-            appending.append(newcombo)
+            new_a_con_count = 0
+            if a == 'a':
+                new_a_con_count = c[0] + 1
+            if new_a_con_count > 2: continue
+            new_l_count = c[1] + (1 if a == 'l' else 0)
+            if new_l_count > 1: continue
+            appending.append((new_a_con_count, new_l_count))
     winning_combos = appending
 
-get_initial_winning_combos()
-day = 4
-for _ in range(27):
-    print(day)
-    extend_winning_combos()
-    day += 1
-print(len(winning_combos))
+def reverse_brute_force() -> int:
+    get_initial_winning_combos()
+    for _ in range(26): extend_winning_combos()
+    return len(winning_combos)
+
+start_time = time.time()
+solution_rbf = reverse_brute_force()
+print(f'Solution space brute force: {solution_rbf} in {time.time()-start_time} seconds')
