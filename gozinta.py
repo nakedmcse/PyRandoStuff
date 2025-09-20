d5f6d5be9111b0adb385d0975a874838a9ce3e88
Walker Aldridge <daldridge@lairdscomputer.com>
2025-09-19 19:32:27 -0500

    Gozinta

---
+++ gozinta.py
@@ -0,0 +1,20 @@
+# Project Euler Problem 548
+# https://projecteuler.net/problem=548
+from functools import cache
+
+@cache
+def gozinta(c: int, target: int) -> int:
+    if c == target: return 1
+    total = 0
+    for i in range(c+1, target + 1):
+        if i % c == 0: total += gozinta(i, target)
+    return total
+
+print(gozinta(1,12))
+print(gozinta(1,48))
+print(gozinta(1,120))
+
+for j in range(2,10000):
+    g = gozinta(1,j)
+    if g == j:
+        print(f'{j} - {g}')
